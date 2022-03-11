1487f8f32a93d39140d515a3ee5ca5a104b950c4
reynald-durans <duransreynald@gmail.com>
2022-03-11 20:29:02 -0300

    adicionando arquivos

--- Aula12/ex011.js
+++ Aula12/ex011.js
@@ -1,8 +1,11 @@
-var idade = 17;
+var idade = 66;
+console.log('________________________');
+console.log(`Você tem ${idade} anos.`);
+console.log('________________________');
 if (idade < 16) {
     console.log('Não vota');
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opicional');
 } else {
-    if (idade < 18) {
-        console.log('Voto opcional');
-    }
+    console.log('Voto obrigatório');
 }
