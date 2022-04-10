52ed917f5c15f81100b2129b37981139c7c02e35
reynald-durans <duransreynald@gmail.com>
2022-04-10 20:48:09 -0300

    adicionando novas aulas

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,12 +1,18 @@
 var c = 1;
-while (c <= 10) {
+do {
+    console.log(`Passo ${c}`);
+    ++c
+} while (c <= 6);
+
+
+/*
+//ESTRUTURA DE REPETIÇÃO - WHILE
+var c = 1;
+while (c <= 500) {
     console.log(`Passo ${c}`);
     c++
 }
-
-
-
-
+*/
 
 /*
 console.log('Tudo bem?');
